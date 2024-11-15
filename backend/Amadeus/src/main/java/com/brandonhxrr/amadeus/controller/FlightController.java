ef7e816e3d09651cceb3782404fdee75b51b6141
Brandon Herrera <brandon.hernandez@encora.com>
2024-11-14 22:56:12 -0600

    Add searchFlights method to retrieve flights according to the required parameters

--- backend/Amadeus/src/main/java/com/brandonhxrr/amadeus/controller/FlightController.java
+++ backend/Amadeus/src/main/java/com/brandonhxrr/amadeus/controller/FlightController.java
@@ -1,15 +1,12 @@
 package com.brandonhxrr.amadeus.controller;
 
-import com.brandonhxrr.amadeus.model.Airport;
-import com.brandonhxrr.amadeus.model.Airports;
-import com.brandonhxrr.amadeus.model.Flight;
+import com.brandonhxrr.amadeus.model.AirportsResponse;
+import com.brandonhxrr.amadeus.model.FlightsResponse;
 import com.brandonhxrr.amadeus.services.AuthService;
 import com.brandonhxrr.amadeus.services.FlightService;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +20,22 @@ public class FlightController {
     }
 
     @GetMapping("/amadeus/airports/search")
-    public ResponseEntity<Airports> searchAirportsByName(@RequestParam String name) {
-        Airports airports = flightService.searchAirportsByName(name).block();
+    public ResponseEntity<AirportsResponse> searchAirportsByName(@RequestParam String name) {
+        AirportsResponse airportsResponse = flightService.searchAirportsByName(name).block();
 
-        return new ResponseEntity<>(airports, HttpStatus.OK);
+        return new ResponseEntity<>(airportsResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/amadeus/flights/search")
+    public ResponseEntity<FlightsResponse> searchFlights(@RequestParam String originLocationCode,
+                                                         @RequestParam String destinationLocationCode,
+                                                         @RequestParam String departureDate,
+                                                         @RequestParam String returnDate,
+                                                         @RequestParam int adults,
+                                                         @RequestParam String currencyCode,
+                                                         @RequestParam Boolean nonStop
+    ) {
+
+        return new ResponseEntity<>(flightService.searchFlights(originLocationCode, destinationLocationCode, departureDate, returnDate, adults, currencyCode, nonStop).block(), HttpStatus.OK);
     }
 }
--- backend/Amadeus/src/main/java/com/brandonhxrr/amadeus/services/FlightService.java
+++ backend/Amadeus/src/main/java/com/brandonhxrr/amadeus/services/FlightService.java
@@ -1,6 +1,7 @@
 package com.brandonhxrr.amadeus.services;
 
-import com.brandonhxrr.amadeus.model.Airports;
+import com.brandonhxrr.amadeus.model.AirportsResponse;
+import com.brandonhxrr.amadeus.model.FlightsResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -13,10 +14,10 @@ public class FlightService {
 
     public FlightService(AuthService authService) {
         this.authService = authService;
-        this.webClient = WebClient.builder().build();
+        this.webClient = WebClient.builder().codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024)).build();
     }
 
-    public Mono<Airports> searchAirportsByName(String airportName) {
+    public Mono<AirportsResponse> searchAirportsByName(String airportName) {
         return authService.getToken()
                 .flatMap(token -> webClient.get()
                         .uri(uriBuilder -> uriBuilder
@@ -29,6 +30,26 @@ public class FlightService {
                                 .build())
                         .header("Authorization", "Bearer " + token)
                         .retrieve()
-                        .bodyToMono(Airports.class));
+                        .bodyToMono(AirportsResponse.class));
+    }
+
+    public Mono<FlightsResponse> searchFlights(String originLocationCode, String destinationLocationCode, String departureDate, String returnDate, int adults, String currencyCode, Boolean nonStop) {
+        return authService.getToken()
+                .flatMap(token -> webClient.get()
+                        .uri(uriBuilder -> uriBuilder
+                                .scheme("https")
+                                .host("test.api.amadeus.com")
+                                .path("v2/shopping/flight-offers")
+                                .queryParam("originLocationCode", originLocationCode)
+                                .queryParam("destinationLocationCode", destinationLocationCode)
+                                .queryParam("departureDate", departureDate)
+                                .queryParam("returnDate", returnDate)
+                                .queryParam("adults", adults)
+                                .queryParam("currencyCode", currencyCode)
+                                .queryParam("nonStop", nonStop)
+                                .build())
+                        .header("Authorization", "Bearer " + token)
+                        .retrieve()
+                        .bodyToMono(FlightsResponse.class));
     }
 }
