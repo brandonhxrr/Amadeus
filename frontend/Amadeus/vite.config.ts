53fa24f9f12d9269b2b063b018787ea3bbab839b
Brandon Herrera <brandon.hernandez@encora.com>
2024-11-15 22:13:13 -0600

    Change server and preview port in vite.config.ts, add proxy server with backend address

--- frontend/Amadeus/vite.config.ts
+++ frontend/Amadeus/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080,
+    proxy: {
+      '/amadeus/': {
+        target: 'http://localhost:9090/',
+        changeOrigin: true
+      }
+    }
+  },
+  preview: {
+    port: 8080
+  },
   plugins: [react()],
 })
